3717fc78469c63901ea5c7f073fed86617632a5a
bren <brendan.sapience@gmail.com>
2016-06-04 19:03:42 -0400

    added functionalities for Statistics, general improvements

--- Automic-RESTful-Server/grails-app/controllers/automic/restful/server/StatisticsController.groovy
+++ Automic-RESTful-Server/grails-app/controllers/automic/restful/server/StatisticsController.groovy
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import com.automic.AECredentials
 import com.automic.DisplayFilters
 import com.automic.ConnectionManager
+import com.uc4.api.DateTime
 import com.uc4.api.SearchResultItem
 import com.uc4.api.StatisticSearchItem
 import com.uc4.api.Task
@@ -42,7 +43,13 @@ class StatisticsController {
 		AllParamMap = [
 			'required_parameters': [],
 			'optional_parameters': [],
-			'optional_filters': ['status (format: filters=[status:1900])','key1 (format: filters=[key1:*.*]','type (format: filters=[type:JOBF])'],
+			'optional_filters': ['status (format: filters=[status:1900])','key1 (format: filters=[key1:*.*]','key2 (format: filters=[key2:*.*]','type (format: filters=[type:JOBF|JOBS])',
+				'alias (format: filters=[alias:*])','client (format: filters=[client:200])','name (format: filters=[name:*])','queue (format: filters=[queue:*])',
+				'runid (format: filters=[runid:*])','status (format: filters=[status:1900])','activation (format: filters=[activation:YYYYMMDDHHMM-YYYYMMDDHHMM])',
+				'start (format: filters=[start:YYYYMMDDHHMM-YYYYMMDDHHMM])','end (format: filters=[end:YYYYMMDDHHMM-YYYYMMDDHHMM])',
+				'platform (format: filters=[platform:WIN|UNIX])',
+				
+				],
 			'required_methods': [],
 			'optional_methods': []
 			]
@@ -61,42 +68,102 @@ class StatisticsController {
 			
 			if(ConnectionManager.runTokenChecks(TOKEN)){
 				com.uc4.communication.Connection conn = ConnectionManager.getConnectionFromToken(TOKEN);
-				//if(FILTERS == null){FILTERS='';}
+				
 				DisplayFilters dispFilters = new DisplayFilters(FILTERS);
 				
 				GenericStatistics req = new GenericStatistics();
-				req.setClient(200);
-				//req.selectAllTypes();
-				req.setTypeJOBF(true);
-				// !!! status MUST following a very strict structure: comma seperated return codes OR range: "1850,1851" or "1800-1899"
-//				if(dispFilters.doesKeyExistInFilter("status")){taskFilter.setStatus(dispFilters.getValueFromKey("status"));}
-//				if(dispFilters.doesKeyExistInFilter("key1")){taskFilter.setArchiveKey1(dispFilters.getValueFromKey("key1"));}
-//				if(dispFilters.doesKeyExistInFilter("type")){
-//					String TYPE = dispFilters.getValueFromKey("type").toUpperCase();
-//					println "Type: " + TYPE;
-//					taskFilter.unselectAllObjects();
-//					switch(TYPE) {
-//						case 'API':taskFilter.setTypeAPI(true);break;
-//						case 'C_HOSTG':taskFilter.setTypeC_HOSTG(true);break;
-//						case 'C_PERIOD':taskFilter.setTypeC_PERIOD(true);break;
-//						case 'CALL':taskFilter.setTypeCALL(true);break;
-//						case 'CPIT':taskFilter.setTypeCPIT(true);break;
-//						case 'EVNT':taskFilter.setTypeEVNT(true);break;
-//						case 'HOSTG':taskFilter.setTypeHOSTG(true);break;
-//						case 'JOBD':taskFilter.setTypeJOBD(true);break;
-//						case 'JOBF':taskFilter.setTypeJOBF(true);break;
-//						case 'JOBG':taskFilter.setTypeJOBG(true);break;
-//						case 'JOBP':taskFilter.setTypeJOBP(true);break;
-//						case 'JOBQ':taskFilter.setTypeJOBQ(true);break;
-//						case 'JOBS':taskFilter.setTypeJOBS(true);break;
-//						case 'JSCH':taskFilter.setTypeJSCH(true);break;
-//						case 'PERIOD':taskFilter.setTypePERIOD(true);break;
-//						case 'REPORT':taskFilter.setTypeREPORT(true);break;
-//						case 'SCRI':taskFilter.setTypeSCRI(true);break;
-//					}
-//				}
+				req.selectAllPlatforms();
+				req.selectAllTypes();
+				req.setDateSelectionNone();
 				
-
+				if(dispFilters.doesKeyExistInFilter("key1")){req.setArchiveKey1(dispFilters.getValueFromKey("key1"));}
+				if(dispFilters.doesKeyExistInFilter("key2")){req.setArchiveKey2(dispFilters.getValueFromKey("key2"));}
+				if(dispFilters.doesKeyExistInFilter("alias")){req.setArchiveKey1(dispFilters.getValueFromKey("alias"));}
+				if(dispFilters.doesKeyExistInFilter("client")){req.setArchiveKey1(dispFilters.getValueFromKey("client"));}
+				if(dispFilters.doesKeyExistInFilter("name")){req.setArchiveKey1(dispFilters.getValueFromKey("name"));}
+				if(dispFilters.doesKeyExistInFilter("queue")){req.setArchiveKey1(dispFilters.getValueFromKey("queue"));}
+				if(dispFilters.doesKeyExistInFilter("runid")){req.setArchiveKey1(dispFilters.getValueFromKey("runid"));}
+				if(dispFilters.doesKeyExistInFilter("status")){req.setArchiveKey1(dispFilters.getValueFromKey("status"));}
+				
+				if(dispFilters.doesKeyExistInFilter("activation")){
+					String RawDate = dispFilters.getValueFromKey("activation");
+					req.setDateSelectionActivation();
+					HandleDateFilter(req, RawDate);
+				}
+				
+				if(dispFilters.doesKeyExistInFilter("end")){
+					String RawDate = dispFilters.getValueFromKey("end");
+					req.setDateSelectionEnd();
+					HandleDateFilter(req, RawDate);
+				}
+				
+				if(dispFilters.doesKeyExistInFilter("start")){
+					String RawDate = dispFilters.getValueFromKey("start");
+					req.setDateSelectionStart();
+					HandleDateFilter(req, RawDate);
+				}
+				
+				// filters=[platform:UNIX|WIN|CIT]
+				if(dispFilters.doesKeyExistInFilter("platform")){
+					req.unselectAllPlatforms();
+					String AllPlatformSelected = dispFilters.getValueFromKey("platform");
+					String[] AllPlatformsArray = AllPlatformSelected.split("\\|");
+					
+					AllPlatformsArray.each {
+							switch(it.toUpperCase()){
+								case ~/WIN|WINDOWS/:req.setPlatformWindows(true);break;
+								case ~/UNX|UNIX|LINUX|REDHAT|UBUNTU|SOLARIS|UX/:req.setPlatformUNIX(true);break;
+								case ~/BS2000|BS/:req.setPlatformBS2000(true);break;
+								case ~/CIT|RA/:req.setPlatformCIT(true);break;
+								case ~/GCO|GCOS8/:req.setPlatformGCOS8(true);break;
+								case ~/JMX/:req.setPlatformJMX(true);break;
+								case ~/MAIL/:req.setPlatformMAIL(true);break;
+								case ~/MPE/:req.setPlatformMPE(true);break;
+								case ~/MVS/:req.setPlatformMVS(true);break;
+								case ~/NSK/:req.setPlatformNSK(true);break;
+								case ~/OA/:req.setPlatformOA(true);break;
+								case ~/OS400|AS400/:req.setPlatformOS400(true);break;
+								case ~/PS/:req.setPlatformPS(true);break;
+								case ~/SIEBEL|SEIBEL/:req.setPlatformSiebel(true);break;
+								case ~/R3/:req.setPlatformR3(true);break;
+								case ~/SQL/:req.setPlatformSQL(true);break;
+								case ~/VMS/:req.setPlatformVMS(true);break;
+								}
+						}
+					}
+				// filters=[type:JOBS|JOBF]
+				if(dispFilters.doesKeyExistInFilter("type")){
+					req.unselectAllTypes();
+					String AllTypesSelected = dispFilters.getValueFromKey("type");
+					String[] AllTypesArray = AllTypesSelected.split("\\|");
+					
+					AllTypesArray.each {
+							switch(it.toUpperCase()){
+									case ~/API/:req.setTypeAPI(true);break;
+									case ~/C_HOSTG/:req.setTypeC_HOSTG(true);break;
+									case ~/C_PERIOD/:req.setTypeC_PERIOD(true);break;
+									case ~/CALL|NOTIFICATION/:req.setTypeCALL(true);break;
+									case ~/CPIT|COCKPIT/:req.setTypeCPIT(true);break;
+									case ~/CLNT|CLIENT/:req.setTypeCLNT(true);break;
+									case ~/EVNT|EVENT/:req.setTypeEVNT(true);break;
+									case ~/EVNT_CHILD|EVENT_CHILD|EVNTCHILD|EVENTCHILD/:req.setTypeEVENT_CHILD(true);break;
+									case ~/HOST|AGENT|NODE/:req.setTypeHOST(true);break;
+									case ~/JOBD/:req.setTypeJOBD(true);break;
+									case ~/JOBF|MFT|FILETRANSFER|TRANSFER/:req.setTypeJOBF(true);break;
+									case ~/JOBG/:req.setTypeJOBG(true);break;
+									case ~/JOBP|JOBPLAN|WORKFLOW|JOBFLOW/:req.setTypeJOBP(true);break;
+									case ~/JOBQ/:req.setTypeJOBQ(true);break;
+									case ~/JOBS|JOB/:req.setTypeJOBS(true);break;
+									case ~/JSCH|SCHEDULE|JOBSCH|SCHED/:req.setTypeJSCH(true);break;
+									case ~/REPORT|REP/:req.setTypeREPORT(true);break;
+									case ~/SCRI|SCRIPT/:req.setTypeSCRI(true);break;
+									case ~/SERV/:req.setTypeSERV(true);break;
+									case ~/SYNC/:req.setTypeSYNC(true);break;
+									case ~/USER/:req.setTypeUSER(true);break;
+								}
+						}
+					}
+				
 				String Msg = CommonAERequests.sendSyncRequestWithMsgReturn(conn,req);
 
 				if(Msg.contains("too many")){ 
@@ -107,17 +174,119 @@ class StatisticsController {
 					render(text: json , contentType: "text/json", encoding: "UTF-8")
 				}else{
 					Iterator<StatisticSearchItem> myIt = req.resultIterator();
-					List<StatisticSearchItem> myList = new ArrayList<StatisticSearchItem>();
-					while(myIt.hasNext()){
-						myList.add(myIt.next());
+					if(myIt != null){
+						List<StatisticSearchItem> myList = new ArrayList<StatisticSearchItem>();
+						while(myIt.hasNext()){
+							myList.add(myIt.next());
+						}
+						
+						JsonBuilder json = CommonJSONRequests.getStatisticResultListAsJSONFormat(myList);
+						render(text: json , contentType: "text/json", encoding: "UTF-8")
+					}else{
+						String json = '{"status":"error","message":"No Data Returned by AE"}'
+						render(text: json , contentType: "text/json", encoding: "UTF-8")
 					}
-					
-					JsonBuilder json = CommonJSONRequests.getStatisticResultListAsJSONFormat(myList);
-					render(text: json , contentType: "text/json", encoding: "UTF-8")
+		
 				}
 				
 			}
 		}
 
+	}
+	
+	def HandleDateFilter(GenericStatistics req, String RawDate){
+		if(RawDate.contains('-')){
+			String RawBeginDate = RawDate.split("-")[0];
+			String RawEndDate = RawDate.split("-")[1];
+			DateTime BeginDate = null;
+			DateTime EndDate = null;
+			
+			String BeginNumberExtracted = RawBeginDate.findAll( /\d+/ )[0] //makes sure we have no other character
+			
+			// Adjusting the length to 14 char
+			if(BeginNumberExtracted.length() == 8){
+				BeginNumberExtracted = BeginNumberExtracted + '000000'
+			}else if(BeginNumberExtracted.length() == 12){
+				BeginNumberExtracted = BeginNumberExtracted + '00'
+			}
+			// if the length is still incorrect.. we just give an arbitrary window. Why? Because..
+			if(BeginNumberExtracted.length() != 14){
+				BeginDate = DateTime.now().addMinutes(-4*60);
+			}else{
+				BeginDate = new DateTime(BeginNumberExtracted.substring(0, 4).toInteger(),BeginNumberExtracted.substring(4, 6).toInteger(),BeginNumberExtracted.substring(6, 8).toInteger(),
+					BeginNumberExtracted.substring(8, 10).toInteger(),BeginNumberExtracted.substring(10, 12).toInteger(),BeginNumberExtracted.substring(12, 14).toInteger())	
+			
+			
+			}
+			
+			if(RawEndDate.toUpperCase() =~/NOW/){
+				EndDate = DateTime.now();
+			} else{
+				String EndNumberExtracted = RawEndDate.findAll( /\d+/ )[0] //makes sure we have no other character
+				
+				// Adjusting the length to 14 char
+				if(EndNumberExtracted.length() == 8){
+					EndNumberExtracted = EndNumberExtracted + '000000'
+				}else if(EndNumberExtracted.length() == 12){
+					EndNumberExtracted = EndNumberExtracted + '00'
+				}
+				// if the length is still incorrect.. we just give an arbitrary window. Why? Because..
+				if(EndNumberExtracted.length() != 14){
+					EndDate = DateTime.now();
+				}else{
+					EndDate = new DateTime(EndNumberExtracted.substring(0, 4).toInteger(),EndNumberExtracted.substring(4, 6).toInteger(),EndNumberExtracted.substring(6, 8).toInteger(),
+						EndNumberExtracted.substring(8, 10).toInteger(),EndNumberExtracted.substring(10, 12).toInteger(),EndNumberExtracted.substring(12, 14).toInteger())
+				}
+			}
+			
+			
+			// 20160101 2100 -201701012100
+			// 20160101-20170101
+			// 201601012100-NOW
+			// LASTNHOURS
+			
+			//println "Begin: " + BeginDate.toString()
+			//println "End: " + EndDate.toString()
+			req.setFromDate(BeginDate);
+			req.setToDate(EndDate);
+
+			
+			
+		}else{ //LASTNHOURS / LASTNMINUTES / LASTNDAYS |  type
+			
+		if(RawDate.toUpperCase() =~ /LAST[0-9]+(YEARS|YEAR|YR|Y|MONTHS|MONTH|MTH|DAYS|DAY|D|HOURS|HR|HOUR|H|MIN|MINUTE|MINUTES|SECONDS|SECOND|SEC|S|)/){
+			
+			String NumberOfUnits = RawDate.findAll( /\d+/ )[0]
+			
+			int Number = NumberOfUnits.toInteger()
+			String Type = RawDate.split(NumberOfUnits)[1]
+			
+			DateTime NOW = DateTime.now()
+			DateTime BEGINNING = DateTime.now()
+			
+			if(Type.toUpperCase() =~/YEARS|YEAR|YR|Y/){
+				BEGINNING.addYears(-Number)
+			}
+			if(Type.toUpperCase() =~/MONTHS|MONTH|MTH/){
+				BEGINNING.addMonth(-Number)
+			}
+			if(Type.toUpperCase() =~/DAY|DAYS|D/){
+				BEGINNING.addDays(-Number)
+			}
+			if(Type.toUpperCase() =~/HOURS|HOUR|HR|H/){
+				BEGINNING.addMinutes(-60*Number)
+			}
+			if(Type.toUpperCase() =~/MIN|MINUTE|MINUTES/){
+				BEGINNING.addMinutes(-Number)
+			}
+			if(Type.toUpperCase() =~/SECONDS|SECOND|SEC|S/){
+				BEGINNING.addSeconds(-Number)
+			}
+
+			req.setFromDate(BEGINNING);
+			req.setToDate(NOW);
+		}
+
+	}
 	}
 }
--- README.md
+++ README.md
@@ -21,13 +21,17 @@ REST API Server for Automic's ONE Automation Platform
 **Important Additional Design Aspects:**
 
      - An authentication token must be passed in every call as a URL parameter (except for the authentication call itself).
-     - Ideally the token will be moved to the request header (to be done..)
+     - The Auth Token can be specified in the request http header (as 'token' parameter), in which case the 'token' url parameter becomes unecessary
      - Tokens are non-persistent (they are not kept if the Server shuts down)
      - Tokens expire after a certain configurable period
      - Ideally the server should be able to return XML responses as well as JSON (JSON only for now) depending on the header content of the request
      - Tokens are uniquely & randomly generated and do not encode any information
      - There needs to be calls dedicated to retrieving available methods / objects & parameters as this REST API will rapidly get more and more complex
      - There needs to be a "commit" mechanism to prevent accidental changes in objects
+     - Filters are designed in a specific way and provided as a url parameter called filters:
+     		filters=[FilterName:FilterValue]
+     		=> the FilterNames available obviously depend on the Object & Action you are working with
+     		=> the FilterValues also depend on the filterName. Some takes only Integers, some UC4Regex, some actual Regex, some have more specific formats
      
      
 **Available Methods & API Categories:**
@@ -50,19 +54,52 @@ REST API Server for Automic's ONE Automation Platform
 * **Search:**
 
      * Supported Objects:
-          * **Jobs**
-               * Filters: name (Format: _filters=[name="NOVA.*"]_)
-          * **Activities**
-               * Filters: status (Format: _filters=[status=1800]_)
-          * **All**
-               * Filters: name (Format: _filters=[name="NOVA.*"]_) 
-               
-     * Examples:
      
-          * /api/awa/search/v1/Jobs?filters=[name:"NOVA."]&token=s9dpur80s8rtvharifrm531387
-          * /api/awa/search/v1/All?filters=[name:"NOVA."]&token=s9dpur80s8rtvharifrm531387
-          * /api/awa/search/v1/Activities?filters=[status:1900]&token=s9dpur80s8rtvharifrm531387
-
+          * **Jobs** 
+               * Required Parameters: 
+               	* name <UC4Regex> (ex: name="NOVA.*")
+               	
+               * Optional Parameters: 
+               	* search_usage <Y|N> (ex: search_usage=Y)
+            
+				ex: * /api/awa/search/v1/Jobs?name="NOVA.*"&token=s9dpur80s8rtvharifrm531387
+				ex: * /api/awa/search/v1/Jobs?name="NOVA.*"&search_usage=Y&token=s9dpur80s8rtvharifrm531387
+                              
+          * **Activities** (Activity Window Content)
+               * Optional Filters:          
+               	* status <Integer|list of Integers|range of Integers> (ex: status:1800 or status:1800,1801,1802 or status:1800-1900)
+               
+          	ex: * /api/awa/search/v1/Activities?filters=[status:1900]&token=s9dpur80s8rtvharifrm531387
+          
+          * **All** (All Objects)
+               * Required Parameters: 
+               	* name <UC4Regex> (ex: name="NOVA.*")
+               	
+               * Optional Parameters: 
+               	* search_usage <Y|N> (ex: search_usage=Y)  
+               	
+				ex: * /api/awa/search/v1/All?name="NOVA.*"&token=s9dpur80s8rtvharifrm531387
+				ex: * /api/awa/search/v1/All?name="NOVA.*"&search_usage=Y&token=s9dpur80s8rtvharifrm531387   
+					             	
+          * **Statistics** (period Window Content)
+               * Optional Filters: 
+               	* name   <UC4Regex>  (ex: ABC.*)
+               	* status <Integer>   (ex: 1900) 
+               	* key1   <UC4Regex>  (ex: ABC.*)
+               	* key2   <UC4Regex>  (ex: ABC.*)
+               	* type   <List of Object Types separated by "|"> (ex: JOBS|JOBP|JOBF)
+               	* alias  <UC4Regex>  (ex: ABC.*)
+               	* client <Integer>   (ex: 200)
+               	* queue  <UC4Regex>  (ex: ABC.*)
+               	* runid  <Integer>   (ex: 123456789001)
+               	* platform <List of Platforms separated by "|"> (ex: WIN|UNIX|CIT)
+               	* activation <((YYYYMMDD|YYYYMMDDHHMM)-(YYYYMMDD|YYYYMMDDHHMM|NOW))|LAST<Integer>(YEARS|MONTHS|DAYS|HOURS|MINUTES|SECONDS)> (ex: 20160601-NOW or 201606012359-201606040000 or LAST45MINUTES or LAST4DAYS)
+               	* start <((YYYYMMDD|YYYYMMDDHHMM)-(YYYYMMDD|YYYYMMDDHHMM|NOW))|LAST<Integer>(YEARS|MONTHS|DAYS|HOURS|MINUTES|SECONDS)> (ex: 20160601-NOW or 201606012359-201606040000 or LAST45MINUTES or LAST4DAYS)
+               	* end <((YYYYMMDD|YYYYMMDDHHMM)-(YYYYMMDD|YYYYMMDDHHMM|NOW))|LAST<Integer>(YEARS|MONTHS|DAYS|HOURS|MINUTES|SECONDS)> (ex: 20160601-NOW or 201606012359-201606040000 or LAST45MINUTES or LAST4DAYS)
+                             
+          ex: * /api/awa/search/v1/Statistics?filters=[platform:WIN|UNIX,type:EVNT|JOBS,name:*,activation:LAST4DAYS]&token=s9dpur80s8rtvharifrm531387
+          ex: * /api/awa/search/v1/Statistics?filters=[platform:WIN,type:JOBS|JOBP,activation:LAST30MINUTES]&token=s9dpur80s8rtvharifrm531387   
+                 
      * returns:
 
           JSON
@@ -70,8 +107,10 @@ REST API Server for Automic's ONE Automation Platform
 * **Rerun:**
 
      * Supported Objects:
+     
           * **Activities**
-               * Mandatory Parameters: runid (Format: _runid=12344556_)
+               * Mandatory Parameters: 
+               	* runid <Integer> (ex: runid=1234567891000)
                
      * Examples:
      
@@ -85,12 +124,16 @@ REST API Server for Automic's ONE Automation Platform
 * **Unblock:**
 
      * Supported Objects:
-          * **Activities**
-               * Mandatory Parameters: runid (Format: _runid=12344556_)
-               
-     * Examples:
      
-          * /api/awa/unblock/v1/Activities?runid=12345678&token=s9dpur80s8rtvharifrm531387
+          * **Activities**
+               * Mandatory Parameters: 
+					* runid <Integer> (ex: runid=1234567891000)
+					* type <JOBS|JOBP> (ex: type=JOBP)
+					
+               * Optional Parameters: 
+					* lnr <Integer> (ex: lnr=0001) (useful when unblocking a task within a workflow that is blocked because of the external taskdependency)
+              
+          ex: * /api/awa/unblock/v1/Activities?runid=12345678&token=s9dpur80s8rtvharifrm531387
 
 
      * returns:
